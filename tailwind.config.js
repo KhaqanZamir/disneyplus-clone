e0c5865693d423712839312d1e85ec2be60a01b7
KhaqanZamir <97103224+KhaqanZamir@users.noreply.github.com>
2022-05-25 18:04:28 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors:{
+        'white' : '#f9f9f9',
+        'blue' : '#0063e5',
+        'blue-1' : '#0483ee',
+        'black' : '#040714',
+        'black-1' : '#090b13',
+      }
+    },
+  },
+  plugins: [],
+}
